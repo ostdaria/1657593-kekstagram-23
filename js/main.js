6ad19d5e798bd6ba72305b3ba9eb9869872f6551
Daria <dashaostapuk98@gmail.com>
2021-06-08 19:20:58 +0300

    временные данные для дальнейшей разработки интерфейса

--- js/main.js
+++ js/main.js
@@ -1,3 +1,4 @@
+// const { get } = require("browser-sync");
 //Функция возвращающая случайное целое число
 
 function getRandomIntegral(min, max) {
@@ -17,3 +18,66 @@ function getLineLength(line) {
   return line.length <= 140;
 }
 getLineLength('Проверяемая строка');
+
+
+
+//Задание module4-task1
+const COUNT_PHOTO = 25;
+const COUNT_COMMENTS = 25;
+
+const NAMES = [
+  'Александр',
+  'Дмитрий',
+  'Андрей',
+  'Артём',
+  'Кирилл',
+  'Михаил',
+  'Никита',
+  'Матвей',
+  'Роман',
+  'Егор',
+  'Иван',
+  'Павел',
+  'Руслан',
+];
+
+const DESCRITPIONS = [
+  'Это портрет мужчины средних лет',
+  'На картине изображен ребенок, стоящий в центре комнаты',
+  'Женщина прогуливается вдоль берега',
+  'Снаружи идет дождь и виднеется дом',
+  'На этой фотографии мы видим человека, играющего на гитаре в парке.',
+];
+
+const MESSAGES = [
+  'Всё отлично!',
+  'В целом всё неплохо. Но не всё.',
+  'Когда вы делаете фотографию, хорошо бы убирать палец из кадра. В конце концов это просто непрофессионально.',
+];
+
+const getRandomArrayElement = (elements) => {
+  return elements[getRandomIntegral(0, elements.length - 1)];
+};
+
+const getRandomComment = () => {
+  return {
+    id: getRandomIntegral(1, 25) ,
+    avatar: 'img/avatar-' + getRandomIntegral(1, 6) + '.svg',
+    message: getRandomArrayElement(MESSAGES),
+    name: getRandomArrayElement(NAMES),
+  }
+};
+
+const commentBlocks = new Array(COUNT_COMMENTS).fill(null).map(() => getRandomComment());
+
+const createPhoto = function () {
+  return {
+    id: getRandomIntegral(1, 25) ,
+    url: 'photos/' + getRandomIntegral(1, 25) + '.jpg',
+    likes: getRandomIntegral(15, 200),
+    description: getRandomArrayElement(DESCRITPIONS),
+    comments: getRandomArrayElement(commentBlocks),
+  }
+};
+
+const photoBlocks = new Array(COUNT_PHOTO).fill(null).map(() => createPhoto());
