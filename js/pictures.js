89c63d114bea244fc6e595838ff146eaf9087086
Daria <dashaostapuk98@gmail.com>
2021-06-21 11:25:38 +0300

    Правки к заданию(отрисовка миниатюр)

--- js/pictures.js
+++ js/pictures.js
@@ -1,18 +1,27 @@
 import {createPhotos} from './data.js';
 
-const pictures = document.querySelector('.pictures');// В контейнер с классом pictures будут добавляться миниатюры картинок
-const pictureTemplate = document.querySelector('#picture').content.querySelector('.picture');// Поиск шаблона по индефикатору #picture и обращаемся к его свойству content чтобы получить доступ к документ фрагменту и находим элемент с классом .picture;
+const picturesElement = document.querySelector('.pictures');
+const pictureTemplateElement = document.querySelector('#picture').content.querySelector('.picture');
 
-const similarPictureTemplate = createPhotos();// Создаем похожие шаблоны миниатюр и вызывем функцию createPhotos и она вернет массив из миниатюр картинок
+const similarPictures = createPhotos();
 
-const similarPictureFragment = document.createDocumentFragment();// Результат метода createDocumentFragment сохраняем в переменную
+const similarPictureFragment = document.createDocumentFragment();
 
-similarPictureTemplate.forEach(({url, likes, comments}) => {
-  const createPictureTemplate = pictureTemplate.cloneNode(true);// Отрисуем шаблон похожей миниатюры и копируем из шаблона необходимые элементы - cloneNode(true)
-  createPictureTemplate.querySelector('.picture__img').src = url;
-  createPictureTemplate.querySelector('.picture__likes').textContent = likes;// У шаблона находим элемент который хотим изменить(.picture__likes) и с помощью метода textContent присваиваем свойство likes шаблону-миниатюре
-  createPictureTemplate.querySelector('.picture__comments').textContent = comments.length;
-  similarPictureFragment.appendChild(createPictureTemplate);// Все шаблоны-миниатюры будем добавлять в similarPictureFragment;
-});// Проходим по коллекцию миниатюр (методом forEach) и отрисовываем их; forEach принимает в качестве параметра функцию колбек и эта функция будет вызвана для каждого элемента массива;
+const renderPicture = ({url, likes, comments}) => {
+  const pictureElement = pictureTemplateElement.cloneNode(true);
+  pictureElement.querySelector('.picture__img').src = url;
+  pictureElement.querySelector('.picture__likes').textContent = likes;
+  pictureElement.querySelector('.picture__comments').textContent = comments.length;
+  similarPictureFragment.appendChild(pictureElement);
 
-pictures.appendChild(similarPictureFragment);// Разово добавим в фрагмент контейнер где будут отрисовываться все шаблоны-миниатюры
+  return pictureElement;
+};
+
+const renderPictures = (pictures) => {
+  const fragment = document.createDocumentFragment();
+  pictures.forEach((picture) => {
+    const picturesElement = renderPicture(picture);
+    fragment.appendChild(picturesElement);
+  });
+  picturesElement.appendChild(fragment);
+};
