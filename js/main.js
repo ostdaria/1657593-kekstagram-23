34b3f4227c4ef78872ab4487eda9bc306db9ff11
Daria <dashaostapuk98@gmail.com>
2021-07-13 21:53:19 +0300

    Доработки по заданию module9-task1

--- js/main.js
+++ js/main.js
@@ -1,9 +1,10 @@
 import './form.js';
-import {createPhotos} from './data.js';
+// import {createPhotos} from './data.js';
 import './utils.js';
-import {renderPictures} from './pictures.js';
+// import {renderPictures} from './pictures.js';
+import './pictures.js';
 import './picture-big.js';
 import './scale-control.js';
 import './edditor-picture.js';
 
-renderPictures(createPhotos());
+// renderPictures(createPhotos());
